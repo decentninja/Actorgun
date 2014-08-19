110674f356e6a7144e0813643c28d8e986552150
sootn <encrypt.wave@gmail.com>
2014-08-19 23:37:36 +0200

    Build

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require("gulp")
 var shell = require("gulp-shell")
 var downloadatomshell = require("gulp-download-atom-shell")
 
-gulp.task("downloadatomshell", function(cb) {
+gulp.task("install", function(cb) {
 	downloadatomshell({
 		version: "0.15.8",
 		outputDir: "binaries"
 	}, cb)
 })
 
-gulp.task("demo", ["downloadatomshell"], shell.task([
+gulp.task("demo", shell.task([
 	"./binaries/Atom.app/Contents/MacOS/Atom app/"
 ]))
