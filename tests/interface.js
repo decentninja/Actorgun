b0a45ef75c8a8aed613d88110e837f528cef1bcd
sootn <encrypt.wave@gmail.com>
2014-09-06 13:45:39 +0200

    More planing

---
+++ tests/interface.js
@@ -0,0 +1,12 @@
+var core = require("../app/core")
+var testhelpers = require("./testhelpers")
+
+describe("Input", function() {
+	it("update html")
+	it("primitives get its html automagicly")
+	it("can be filled from html")
+})
+
+describe("Output", function() {
+	it("update html")
+})
