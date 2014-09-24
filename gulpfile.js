a447aaec856ae24e7c4b6aac7e2fe6293c983e5b
sootn <encrypt.wave@gmail.com>
2014-09-24 23:46:39 +0200

    Gulpfile changes to support less

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require("gulp")
 var shell = require("gulp-shell")
 var downloadatomshell = require("gulp-download-atom-shell")
-var jasmine = require('gulp-jasmine')
+var jasmine = require("gulp-jasmine")
+var less = require("gulp-less")
+var path = require("path")
+var sourcemaps = require("gulp-sourcemaps")
+var watch = require("gulp-watch")
+var plumber = require('gulp-plumber');
 
 gulp.task("install", function(cb) {
 	downloadatomshell({
@@ -10,12 +15,24 @@ gulp.task("install", function(cb) {
 	}, cb)
 })
 
-gulp.task("demo", shell.task([
+gulp.task('less', function () {
+  gulp.src("app/**/*.less")
+  	.pipe(plumber())
+  	.pipe(watch("app/**/*.less"))
+	.pipe(sourcemaps.init())
+	.pipe(less())
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest('app'))
+});
+
+gulp.task("demo", ["less"], shell.task([
 	"./binaries/Atom.app/Contents/MacOS/Atom app/"
 ]))
 
 gulp.task('test', function () {
-    return gulp.src('tests/test.js').pipe(jasmine({
-    	includeStackTrace: true
-    }))
+	return gulp.src('tests/test.js').pipe(jasmine({
+		includeStackTrace: true
+	}))
 })
+
+gulp.task("default", ["demo"])
