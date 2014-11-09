3653221efd6483a2cc1552ed7ad696fb55913185
sootn <encrypt.wave@gmail.com>
2014-11-09 16:45:09 +0100

    Baby steps

--- app/elements/part-part/part-part.js
+++ app/elements/part-part/part-part.js
@@ -1,11 +1,18 @@
 Polymer({
-	part: new PartPart("Name"),
+	part: new PartPart("Name", [new InternalPart([], [], function() {})]),
 	columns: [
-		[{}, {}, {}],	// parts
+		[{name: "blu"}, {}, {}],	// parts
 		[{}, {}],
 		[{}],
 	],
 	ready: function() {
-		
+		this.part.parts.forEach(function(part) {
+			var ndeps = 0 // TODO calculate # part dependencies
+			if(this.columns[ndeps]) {
+				this.columns[ndeps].push(part)
+			} else {
+				this.columns[ndeps] = [part]
+			}
+		}, this)
 	}
 })
